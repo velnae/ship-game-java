caf0fe888feb45ff88fb5041f57bd468c07ed7e2
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2023-06-15 09:09:23 -0500

    Feat: 15-06-2023 - Definición de propiedades compartidas.
    Propiedades repetitivas en todos los objetos del juego.

---
+++ src/main/java/com/codideep/app/generic/ObjectAttribute.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codideep.app.generic;
+
+/**
+ *
+ * @author KAAF0
+ */
+public abstract class ObjectAttribute {
+
+    public static int speed = 0;
+    public static int[] position = {0, 0};
+    public static int[] dimension = {0, 0};
+}
---
+++ src/main/java/com/codideep/app/object/Ship.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codideep.app.object;
+
+import com.codideep.app.generic.ObjectAttribute;
+
+/**
+ *
+ * @author KAAF0
+ */
+public class Ship extends ObjectAttribute {
+
+}
