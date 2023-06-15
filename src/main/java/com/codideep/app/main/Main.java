ed044ae7951de4801e219bd2486f78af7a7cdd9a
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2023-06-15 08:26:32 -0500

    Build: Init.
    Inicialización.

---
+++ src/main/java/com/codideep/app/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.codideep.app.main;
+
+/**
+ *
+ * @author KAAF0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola en el curso de pp.");
+    }
+}
